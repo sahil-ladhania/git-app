3df2c5a892d81ace268e360c94e366c1d604edfa
Sahil <sahilladhania5@gmail.com>
2023-12-18 19:01:48 +0530

    Made Card Component.

---
+++ src/Components/Card.js
@@ -0,0 +1,17 @@
+// Card.js
+
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div className="card">
+            {/* Card Content Goes Here */}
+            <h1>1441 Pizzeria</h1>
+            <p>
+                Famous Shop for Excellent Pizzas...
+            </p>
+        </div>
+    );
+};
+
+export default Card;
